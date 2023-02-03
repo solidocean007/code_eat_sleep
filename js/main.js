17291db5d4a2c42a3c4fe18df21f2f4cf1a3627e
solidocean007 <clintonwilliams007@gmail.com>
2023-02-03 16:05:58 -0500

    JS to switch theme colors

--- js/main.js
+++ js/main.js
@@ -14,25 +14,60 @@ const isVisible = 'is-visible';
 const root = document.documentElement;
 
 /* Theme */
-const toggleTheme = document.querySelector(themTab);
+const toggleTheme = document.querySelector(themeTab);
 const switcher = document.querySelectorAll(switcherBtn);
 const currentTheme = localStorage.getItem(theme);
 
-
+/* Modal */
 const openModal = document.querySelectorAll(modalOpen);
 const closeModal = document.querySelectorAll(modalClose);
 
-// Full Site Modal "open buttons"
-for(const el of openModal){
-  el.addEventListener('click', function(){
-    const modalId = this.dataset.open;
-    document.getElementById(modalId).classList.add(isVisible);
+const setActive = () => (el, selector) => {
+  if(document.querySelector(`${selector}.${active}`) !== null) {
+    document.querySelector(`${selector}.${active}`).classList.remove(active);
+  }else {
+    el.classList.add(active);
+  }
+}
+
+const setTheme = (val) => {
+  if (val === dark) {
+    root.setAttribute(dataTheme, dark);
+    localStorage.setItem(theme, dark);
+  } else {
+    root.setAttribute(dataTheme, light);
+    localStorage.setItem(theme, light);
+  }
+};
+
+toggleTheme.addEventListener("click", function() {
+  const tab = this.parentElement.parentElement;
+  if (!tab.className.includes(open)) {
+    tab.classList.add(open);
+  } else {
+    tab.classList.remove(open);
+  }
+});
+
+for (const el of switcher) {
+  el.addEventListener('click', function() {
+    const toggle = this.dataset.toggle;
+    setActive(el, switcherBtn);
+    setTheme(toggle);
   })
 }
 
 // Full Site Modal "open buttons"
-for(const el of closeModal){
-  el.addEventListener('click', function(){
+for (const el of openModal) {
+  el.addEventListener("click", function () {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+  });
+}
+
+// Full Site Modal "open buttons"
+for (const el of closeModal) {
+  el.addEventListener("click", function () {
     this.parentElement.parentElement.classList.remove(isVisible);
-  })
+  });
 }
