d3c12cb995dab08f901b953d3566c801ec98b94b
solidocean007 <clintonwilliams007@gmail.com>
2023-02-16 12:49:41 -0500

    Style carousel slides

--- js/carousel.js
+++ js/carousel.js
@@ -2,27 +2,23 @@ const slides = document.querySelectorAll('.review-item');
 const buttons = document.querySelectorAll('.slide-ctrl-container button');
 
 let current = Math.floor(Math.random() * slides.length);
-console.log(current);
 let next = current < slides.length - 1 ? current + 1: 0;
 let prev = current > 0 ? current -1 : slides.length -1;
 
-/* create eventListener for prev/next */
-// goToNext()
-// goToPrev()
-// updateIndexes(param)
-// updateCSS()
-
-// -- decide how to call prev/next
-// -- update variables
-// -- [current] = newIndex
-// -- [next] current + 1 : 0
-// -- [prev] current -1 : length -1
-// -- update the css 
+const update = () => {
+  slides.forEach((slide) => {
+    slide.classList.remove('active', 'prev', 'next');
+  })
+  slides[current].classList.add('active');
+  slides[prev].classList.add('prev');
+  slides[next].classList.add('next');
+};
 
 const goToNum = (number) => {
   current = number;
   next = current < slides.length - 1 ? current + 1: 0;
   prev = current > 0 ? current -1 : slides.length -1;
+  update();
 }
 
 const goToNext = () => current < slides.length - 1 ? goToNum(current + 1): goToNum(0);
@@ -31,3 +27,5 @@ const goToPrev = () => current > 0 ? goToNum(current -1) : goToNum(slides.length
 for (let i = 0; i < buttons.length; i += 1) {
   buttons[i].addEventListener('click', () => i === 0 ? goToPrev() : goToNext());
 }
+
+update();
