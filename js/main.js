142a0006feb007e1dea00bd82c41db7c5144ac05
solidocean007 <clintonwilliams007@gmail.com>
2023-02-18 06:05:32 -0500

    Marquee animation

--- js/main.js
+++ js/main.js
@@ -346,3 +346,12 @@ document.addEventListener("keyup", (e) => {
     
   }
 });
+
+const elmsDisplayed = getComputedStyle(root).getPropertyValue('--marquee-elms-displayed');
+const marqueeContent = document.querySelector('ul.marquee-content');
+
+root.style.setProperty('--marquee-elms', marqueeContent.children.length);
+
+for( let i = 0; i < elmsDisplayed; i += 1) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
